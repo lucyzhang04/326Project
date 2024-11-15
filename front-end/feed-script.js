a6ad3797353d55d553498947888d7ea5dda2a6aa
saadhvir <saadhvir@gmail.com>
2024-11-14 21:09:41 -0500

    Added image display to widgets
    Added img elements for each widgets, allowing corresponding image to be displayed for each submission. Added styling to styles.css to properly position other elements around image.

--- front-end/feed-script.js
+++ front-end/feed-script.js
@@ -10,25 +10,57 @@ const getSubmissions = async () => {
 
 const submissions = [
     { 
-        "name": "Cotton-Eyed Joe\n",
+        "name": "The Rest is History",
         "host" : "Jane Doe\n",
-        "image": "This is a mock response", 
+        "image": "https://ichef.bbci.co.uk/images/ic/1424x801/p0d0mjrz.jpg.webp", 
     }, 
     { 
-        "name": "Podcast 2\n",
-        "host" : "John Deere\n",
-        "image": "This is a mock response", 
+        "name": "The Daily",
+        "host" : "New York Times",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04154511/Free-Stock-Photos-06.jpg"
     },
     { 
-        "name": "How to Save the Planet\n",
-        "host" : "Jane Doe\n",
-        "image": "This is a mock response", 
+        "name": "How to Save the Planet",
+        "host" : "Jane Doe",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg", 
     },
     { 
         "name": "Podcast 1\n",
-        "host" : "wfjwoejgwejgioerigoergjorigj\n",
-        "image": "This is a mock response", 
-    }
+        "host" : "Jack & Jill",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04160429/Free-Stock-Photos-07.jpg", 
+    },
+    { 
+        "name": "How to Save the Planet",
+        "host" : "Jane Doe",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg", 
+    },
+    { 
+        "name": "The Rest is History",
+        "host" : "Jane Doe\n",
+        "image": "https://ichef.bbci.co.uk/images/ic/1424x801/p0d0mjrz.jpg.webp", 
+    }, 
+    { 
+        "name": "The Daily",
+        "host" : "New York Times",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04154511/Free-Stock-Photos-06.jpg"
+    },
+    { 
+        "name": "How to Save the Planet",
+        "host" : "Jane Doe",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg", 
+    },
+    { 
+        "name": "Podcast 1\n",
+        "host" : "Jack & Jill",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04160429/Free-Stock-Photos-07.jpg", 
+    },
+    { 
+        "name": "The Daily",
+        "host" : "New York Times",
+        "image": "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04154511/Free-Stock-Photos-06.jpg"
+    },
+
+
 
 ]; 
 //building basic screen layout 
@@ -45,30 +77,41 @@ const build = () => {
     for(let x = 0; x < rows; x++){
         for(let y = 0; y < columns; y++){
             if(idx == submissions.length) continue;
+            //creating elements 
             const widget = document.createElement("div"); 
             const infoContainer = document.createElement("div"); 
             const infoList = document.createElement("ul");
+            const img = document.createElement("img"); 
 
             const heartButton = document.createElement("button");
             const plus =  document.createTextNode("+");
             const nameText = document.createElement("li");
             const hostText = document.createElement("li");
+
             
+            //filling out text Nodes and image info
             nameText.innerText = submissions[idx]["name"];
             hostText.innerText = submissions[idx]["host"];
+            img.src = submissions[idx]["image"];;
 
-            nameText.classList.add("list-element");
-            hostText.classList.add("list-element");
+            //adding classes 
+            nameText.classList.add("name-element");
+            hostText.classList.add("host-element");
+            img.classList.add("img-square");
             infoList.classList.add("info-list");
             infoContainer.classList.add("info-container");
             widget.classList.add("widget"); 
             heartButton.classList.add("heart-button");
 
+            //adding event listener 
+            heartButton.addEventListener("click", () => alert(`${nameText.innerText} has been saved to liked songs!`));
+            //appending children 
             heartButton.appendChild(plus); 
             infoList.appendChild(nameText); 
             infoList.appendChild(hostText); 
             infoContainer.appendChild(infoList); 
-            infoContainer.appendChild(heartButton)
+            infoContainer.appendChild(heartButton);
+            widget.appendChild(img);
             widget.appendChild(infoContainer); 
             grid.appendChild(widget); 
             ++idx;
