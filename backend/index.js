2373d69eeec5f5918d53f54267ce87a0fc21cccb
Lucy Zhang <lpzhang@umass.edu>
2024-12-04 17:01:31 -0500

    Modify trending-routes.js to export router.

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,9 @@ const songRoutes = require("./routes/feed-routes.js");
 app.use("/feed", songRoutes);
 app.use("/spotify", spotifyRoutes);
 
+const summaryStatRoutes = require("./routes/trending-routes.js");
+app.use('/trending', summaryStatRoutes);
+
 const socket = new WebSocket.Server({port: 9000}); 
 
 socket.on("connection", (s) => {
@@ -31,8 +34,6 @@ socket.on("connection", (s) => {
     })
 });
 
-const summaryStatRoutes = require("./routes/trending-routes.js");
-app.use('/trending', summaryStatRoutes);
 
 //For now I am initializing the database in the default route. We should eventually add a routes file similar to Tasks V5.
 app.get("/", async (req, res) => {
